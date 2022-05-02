4613e3656e890bd885271eb8fcfb7c134546539a
JuaumZz <cover4772@gmail.com>
2022-05-02 15:49:48 -0300

    Updated

--- src/Commands/Info/botinfo.js
+++ src/Commands/Info/botinfo.js
@@ -56,7 +56,7 @@ module.exports = class extends Command {
     .setThumbnail(this.client.user.displayAvatarURL({ dynamic: true }))
     .setDescription(`Opa, tudo bom? Sou o Astro Music e estou aqui para melhorar a diversão em seu servidor com a música. Fui feito com [Discord.js](https://discord.js.org/#/) e estou usando o [Lavalink](https://github.com/davidffa/lavalink/releases) para tocar as músicas. Me adicione no seu servidor e vamos embarcar em uma viagem para este novo planeta chamado Música.`)
     .addFields([
-      { name: "<:id:970752418461663263> Sobre", value: `> <:bot:970750198219427901> Nome: \`${this.client.user.tag}\`\n> <:dev:970750017495236768> Desenvolvedor: \`${owner}\`\n> <:ping:970754837711044658> Ping: \`${this.client.ws.ping.toFixed(2)}ms\`\n> <:slash:970750510174986320> Slash: \`/\``, inline: false },
+      { name: "<:id:970752418461663263> Sobre", value: `> <:bot:970750198219427901> Nome: \`${this.client.user.tag}\`\n> <:dev:970750017495236768> Desenvolvedor: \`${owner}\`\n> <:ping:970754837711044658> Ping: \`${this.client.ws.ping.toFixed(2)}ms\`\n> <:slash:970750510174986320> Slash: \`/\`\n> <:uptime:970758956760846367> Online há: ${uptime}`, inline: false },
       { name: "<:info:970750652332527697> Informações", value: `> <:servers:970751951736307822> Servidores: \`${guilds}\`\n> <:users:970752089934426153> Membros: \`${users}\`\n> <:chats:970752208155078666> Chats: \`${channels}\``, inline: false },
       { name: "<:lavalink:970757500485894154> Lavalink", value: `> <:playing:970751016326811648> Tocando: \`${playing}\`\n> <:node:970750775833804880> Nodes: \`${nodes}\``, inline: false },
     ])
