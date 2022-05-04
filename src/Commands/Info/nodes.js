d366c06998d242da06b291243213efdbff9031bc
JuaumZz <cover4772@gmail.com>
2022-05-03 23:49:57 -0300

    Updated

--- src/Commands/Info/nodes.js
+++ src/Commands/Info/nodes.js
@@ -17,12 +17,27 @@ module.exports = class extends Command {
     let text = '';
 
     for (let i = 0; i < nodes.length; i++) {
+      const state = nodeSates(nodes[i].state)
       pingnode = await nodes[i].ping()
       uptime = await this.client.util.msToDate(await nodes[i].stats.uptime)
-      text += `( ${await nodes[i].identifier} ) Tocando: ${await nodes[i].stats.playingPlayers} | Players: ${await nodes[i].stats.players} | Ping: ${pingnode}ms | Uptime: ${uptime}\n`
+      text += `${state} ( ${await nodes[i].identifier} ) Tocando: ${await nodes[i].stats.playingPlayers} | Players: ${await nodes[i].stats.players} | Ping: ${pingnode}ms | Uptime: ${uptime}\n`
     }
 
     interaction.reply({ content: `\`\`\`\n${text}\`\`\`` })
 
+  }
+}
+
+function nodeSates(states) {
+  switch(states) {
+    case 0 : { 
+      return '🟠';
+    }
+    case 1 : {
+      return '🟢'
+    }
+    case 2 : {
+      return '🔴'
+    }
   }
 }
